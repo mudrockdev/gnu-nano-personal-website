8d2299915e16c02f6d9f4390aac507d2bd593019
Ufuk Furkan Öztürk <ufukfurkanozturk@gmail.com>
2025-02-08 19:28:30 +0300

    added THE CUBE

---
+++ src/lib/TheCube.svelte
@@ -0,0 +1,102 @@
+<script lang="ts">
+	import SuzuranAngy from '../assets/suzuran-angy.gif';
+	import SuzuranHappy from '../assets/suzuran-happy.gif';
+	import SuzuranTail from '../assets/suzuran-tail.gif';
+	import VulpisfogliaCry from '../assets/vulpisfoglia-cry.gif';
+	import VulpisfogliaGroom from '../assets/vulpisfoglia-groom.gif';
+	import VulpisfogliaPleading from '../assets/vulpisfoglia-pleading.gif';
+</script>
+
+<div class="container">
+	<div class="prism">
+		<div class="face front">
+			<img src={SuzuranAngy} alt="" />
+		</div>
+		<div class="face back">
+			<img src={VulpisfogliaCry} alt="" />
+		</div>
+		<div class="face right">
+			<img src={SuzuranHappy} alt="" />
+		</div>
+		<div class="face left">
+			<img src={VulpisfogliaGroom} alt="" />
+		</div>
+		<div class="face top">
+			<img src={SuzuranTail} alt="" />
+		</div>
+		<div class="face bottom">
+			<img src={VulpisfogliaPleading} alt="" />
+		</div>
+	</div>
+</div>
+
+<style>
+	.container {
+		width: 200px;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.prism {
+		position: relative;
+		width: 200px;
+		height: 200px;
+		transform-style: preserve-3d;
+		animation: rotate 10s infinite linear;
+	}
+
+	.face {
+		position: absolute;
+		width: 200px;
+		height: 200px;
+		background-color: rgba(255, 255, 255, 1);
+		border: 2px solid #f5f5f5;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		font-size: 20px;
+		color: white;
+		border-radius: 10px;
+
+		& img {
+			width: 100%;
+			height: 100%;
+			object-fit: cover;
+			border-radius: 10px;
+		}
+	}
+
+	.front {
+		transform: matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 100, 1);
+	}
+
+	.back {
+		transform: matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, -100, 1);
+	}
+
+	.right {
+		transform: matrix3d(0, 0, 1, 0, 0, 1, 0, 0, -1, 0, 0, 0, 100, 0, 0, 1);
+	}
+
+	.left {
+		transform: matrix3d(0, 0, 1, 0, 0, 1, 0, 0, -1, 0, 0, 0, -100, 0, 0, 1);
+	}
+
+	.top {
+		transform: matrix3d(1, 0, 0, 0, 0, 0, 1, 0, 0, -1, 0, 0, 0, 100, 0, 1);
+	}
+
+	.bottom {
+		transform: matrix3d(1, 0, 0, 0, 0, 0, 1, 0, 0, -1, 0, 0, 0, -100, 0, 1);
+	}
+
+	@keyframes rotate {
+		from {
+			transform: rotateX(0) rotateY(0) rotateZ(0);
+		}
+		to {
+			transform: rotateX(360deg) rotateY(360deg) rotateZ(360deg);
+		}
+	}
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,10 +2,24 @@
 	import { QueryClient, QueryClientProvider } from '@tanstack/svelte-query';
 	import Option from '$lib/Option.svelte';
 	import ErrorPage from './+error.svelte';
+	import TheCube from '$lib/TheCube.svelte';
 	import { page } from '$app/stores';
 	import './tailwind.css';
-
 	const queryClient = new QueryClient();
+	let weebMode = false;
+	if (localStorage.getItem('weebMode') === null || localStorage.getItem('weebMode') === 'false') {
+		weebMode = false;
+	} else {
+		weebMode = true;
+	}
+
+	function setWeebMode() {
+		weebMode = !weebMode;
+
+		weebMode === true
+			? localStorage.setItem('weebMode', 'true')
+			: localStorage.setItem('weebMode', 'false');
+	}
 </script>
 
 {#if $page.status === 404}
@@ -19,41 +33,57 @@
 		<main class="max-w-screen max-h-screen">
 			<div class="fixed w-screen">
 				<div class="flex justify-between px-8 bg-[#3465A4]" id="nav">
-					<p>GNU nano 8.1</p>
+					<p>GNU nano 8.2</p>
 					<p>www.mudrock.dev</p>
-					<p>I</p>
+					<div class="flex">
+						I
+						<a
+							class="ml-4"
+							href={'#'}
+							on:click={() => {
+								setWeebMode();
+							}}>x</a
+						>
+					</div>
 				</div>
 			</div>
-			<slot />
-			<div class="fixed bottom-0 w-screen bg-black" id="footer">
-				<div class="flex justify-between pr-32">
-					<div>
-						<Option optionKey="^G" optionName="Help" />
-						<Option optionKey="^X" optionName="Exit" />
+			<div class="flex">
+				<slot />
+				{#if weebMode}
+					<div class="ml-auto mt-32 mr-32">
+						<TheCube class="fixed right-0 bottom-0" />
 					</div>
-					<div>
-						<Option optionKey="^O" optionName="Write Out" />
-						<Option optionKey="^R" optionName="Read File" />
-					</div>
-					<div>
-						<Option optionKey="^F" optionName="Where Is" />
-						<Option optionKey={`^\\`} optionName="Replace" />
-					</div>
-					<div>
-						<Option optionKey="^K" optionName="Cut" />
-						<Option optionKey="^U" optionName="Paste" />
-					</div>
-					<div>
-						<Option optionKey="^T" optionName="Execute" />
-						<Option optionKey="^J" optionName="Justify" />
-					</div>
-					<div>
-						<Option optionKey="^C" optionName="Location" />
-						<Option optionKey="^/" optionName="Go To Line" />
-					</div>
-					<div>
-						<Option optionKey="^M-U" optionName="Undo" wideBG="true" />
-						<Option optionKey="^M-E" optionName="Redo" wideBG="true" />
+				{/if}
+				<div class="fixed bottom-0 w-screen bg-black" id="footer">
+					<div class="flex justify-between pr-32">
+						<div>
+							<Option optionKey="^G" optionName="Help" />
+							<Option optionKey="^X" optionName="Exit" />
+						</div>
+						<div>
+							<Option optionKey="^O" optionName="Write Out" />
+							<Option optionKey="^R" optionName="Read File" />
+						</div>
+						<div>
+							<Option optionKey="^F" optionName="Where Is" />
+							<Option optionKey={`^\\`} optionName="Replace" />
+						</div>
+						<div>
+							<Option optionKey="^K" optionName="Cut" />
+							<Option optionKey="^U" optionName="Paste" />
+						</div>
+						<div>
+							<Option optionKey="^T" optionName="Execute" />
+							<Option optionKey="^J" optionName="Justify" />
+						</div>
+						<div>
+							<Option optionKey="^C" optionName="Location" />
+							<Option optionKey="^/" optionName="Go To Line" />
+						</div>
+						<div>
+							<Option optionKey="^M-U" optionName="Undo" wideBG="true" />
+							<Option optionKey="^M-E" optionName="Redo" wideBG="true" />
+						</div>
 					</div>
 				</div>
 			</div>
